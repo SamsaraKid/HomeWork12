1ee89e1a802b48d8a59ecc9bf8665cd14167e0f3
koany <koroliev@yandex.ru>
2023-04-04 18:52:57 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+let start_number = 1
+let end_number = 15
+let attempts_number = 3
+let attempts = 0
+
+let ansver = Math.floor(Math.random() * (Math.abs(start_number - end_number) + 1)) + Math.min(start_number,end_number)
+console.log(ansver)
+
+
+function guess(){
+    let variable = Number($('#input_field').val())
+    if (attempts < 3) {
+        if (variable < ansver){
+            $('#result').text('Ваше число меньше ответа')
+            attempts+=1
+        }   else if (variable > ansver){
+                $('#result').text('Ваше число больше ответа')
+                attempts+=1
+        }   else {
+                $('#result').text('Вы угадали!')
+        }
+    }    else {
+            $('#result').text('Попытки исчерпаны :(')
+    }
+    $('#input_field').val('')
+}
+
+$('#guess_button').click(guess)
