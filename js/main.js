d344ba9c80d480ef6df6ca05f0fb093b2595c263
koany <koroliev@yandex.ru>
2023-04-05 18:17:12 +0300

    Готовый простой вариант

--- js/main.js
+++ js/main.js
@@ -2,14 +2,14 @@ let start_number = 1
 let end_number = 15
 let attempts_number = 3
 let attempts = 0
-
 let ansver = Math.floor(Math.random() * (Math.abs(start_number - end_number) + 1)) + Math.min(start_number,end_number)
 console.log(ansver)
 
 
+
 function guess(){
     let variable = Number($('#input_field').val())
-    if (attempts < 3) {
+    if (attempts < 2) {
         if (variable < ansver){
             $('#result').text('Ваше число меньше ответа')
             attempts+=1
@@ -21,8 +21,11 @@ function guess(){
         }
     }    else {
             $('#result').text('Попытки исчерпаны :(')
+            // ansver = Math.floor(Math.random() * (Math.abs(start_number - end_number) + 1)) + Math.min(start_number,end_number)
+            // attempts = 0
     }
     $('#input_field').val('')
 }
+
 
 $('#guess_button').click(guess)
